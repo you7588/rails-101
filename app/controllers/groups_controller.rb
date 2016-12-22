f1c3af7512033606c2414fa6f8c202c321e36423
你的名字 <pincha120@gmail.com>
2016-12-22 21:33:01 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,14 +2,20 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
   def new
-   @group = Group.new
+    @group = Group.new
   end
+
+ def show
+   @group = Group.find(params[:id])
+ end
+
   def create
-     @group = Group.new(group_params)
+    @group = Group.new(group_params)
     @group.save
 
-      redirect_to groups_path
+    redirect_to groups_path
   end
 
   private
