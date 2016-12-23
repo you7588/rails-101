2212ca3f8bb00019e72a7c1dbe0e5c5f654413af
你的名字 <pincha120@gmail.com>
2016-12-23 19:33:25 +0800

    people can't see edit button unless he is group owner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-+//= require turbolinks
+//= require turbolinks
 //= require_tree .
 //= require bootstrap/alert
- //= require bootstrap/dropdown
+//= require bootstrap/dropdown
